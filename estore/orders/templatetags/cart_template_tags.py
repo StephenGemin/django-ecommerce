d51c5ba07ebe4c8e5901c921b3ca9c1824e84279
StephenGemin <45926479+StephenGemin@users.noreply.github.com>
2021-09-09 21:55:31 -0400

    Comment code in tag cart_item_count ... affecting page load time
    Profiling revealed that querying every page refresh is time expensive.  Sometimes adding an extra 1+ second to each page load.
    
    Will need to investigate an alternative that is faster.

--- estore/orders/templatetags/cart_template_tags.py
+++ estore/orders/templatetags/cart_template_tags.py
@@ -7,8 +7,12 @@ register = template.Library()
 
 @register.filter
 def cart_item_count(user):
-    if user.is_authenticated:
-        qs = Order.objects.filter(user=user, ordered=False)
-        if qs.exists():
-            return qs[0].items.count()
-    return 0
+    # TODO: implement faster alternative.  The code below results in an extra 1+ sec
+    #  for every page load/refresh.
+    #  It appears the exist and count check are taking the most time ... based on
+    #  profiling results from the django debug toolbar
+    # if user.is_authenticated:
+    #     qs = Order.objects.filter(user=user, ordered=False)
+    #     if qs.exists():
+    #         return qs[0].items.count()
+    return 1
