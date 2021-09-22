12095866258cd4859731f2ae49fd33d826154301
StephenGemin <45926479+StephenGemin@users.noreply.github.com>
2021-09-21 23:00:00 -0400

    add custom fields within admin view
    Order, OrderItem and BillingAddress

--- estore/shopping/admin.py
+++ estore/shopping/admin.py
@@ -2,8 +2,21 @@ from django.contrib import admin
 
 from . import models as m
 
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'ordered', 'ordered_date']
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['item', 'quantity', 'ordered', 'user']
+
+
+class BillAddressAdmin(admin.ModelAdmin):
+    list_display = ["user", "address", "address2", "country", "postal_code"]
+
+
 admin.site.register(m.Item)
-admin.site.register(m.Order)
-admin.site.register(m.OrderItem)
-admin.site.register(m.BillingAddress)
+admin.site.register(m.Order, OrderAdmin)
+admin.site.register(m.OrderItem, OrderItemAdmin)
+admin.site.register(m.BillingAddress, BillAddressAdmin)
 admin.site.register(m.Payment)
