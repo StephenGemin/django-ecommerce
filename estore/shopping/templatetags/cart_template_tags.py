bf343cb657d2c0d31e40e46adb2a92e091a775dc
StephenGemin <45926479+StephenGemin@users.noreply.github.com>
2021-10-03 12:54:44 -0400

    fix cart showing correct item count in NavBar

--- estore/shopping/templatetags/cart_template_tags.py
+++ estore/shopping/templatetags/cart_template_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 from ..models import Order
 
@@ -7,12 +8,22 @@ register = template.Library()
 
 @register.filter
 def cart_item_count(user):
-    # TODO: implement faster alternative.  The code below results in an extra 1+ sec
-    #  for every page load/refresh.
-    #  It appears the exist and count check are taking the most time ... based on
-    #  profiling results from the django debug toolbar
+    if user.is_authenticated:
+        try:
+            order = Order.objects.get(user=user, ordered=False)
+            return order.items.count()
+        except (ObjectDoesNotExist, MultipleObjectsReturned):
+            return 0
+
+    # NOTE:
+    # The following code was very slow.  It was causing an extra 1-2+ sec for
+    # every page load/refresh. From django debug-toolbar, the exist and count checks
+    # were taking the most time.  Especially the count.
+    # Adding these here as a note to future self, as this took a while to figure out.
+    # The code below was from the YouTube tutorial.
+
+    # CODE BLOCK
     # if user.is_authenticated:
     #     qs = Order.objects.filter(user=user, ordered=False)
     #     if qs.exists():
     #         return qs[0].items.count()
-    return 1
